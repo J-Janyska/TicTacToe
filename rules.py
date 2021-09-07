6a55afbb11c5a8ca141066f07a0f90394a1434db
J-Janyska <88319948+J-Janyska@users.noreply.github.com>
2021-09-07 15:48:48 +0200

    Add files via upload

---
+++ rules.py
@@ -0,0 +1,24 @@
+TICTACTOE_RULES = """
+        Welcome to Tic Tac Toe !
+========================================
+             GAME RULES:
+Each player can place one mark (or stone)
+per turn on the 3x3 grid. The WINNER is
+who succeeds in placing three of their
+marks in a:
+* horizontal,
+* vertical or
+* diagonal row
+
+Move numbers:
++---+---+---+
+| 1 | 2 | 3 |
++---+---+---+
+| 4 | 5 | 6 |
++---+---+---+
+| 7 | 8 | 9 |
++---+---+---+
+
+========================================
+Let's start the game !
+"""
